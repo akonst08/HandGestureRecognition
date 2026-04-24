b0c419bda3e2035c9a1a450ec359b4ce1f12212d
akonst08 <akonst08@ucy.ac.cy>
2026-04-24 16:03:29 +0300

    Pin dependencies and switch keypoint classifier to Keras model

--- model/keypoint_classifier/keypoint_classifier.py
+++ model/keypoint_classifier/keypoint_classifier.py
@@ -7,30 +7,15 @@ import tensorflow as tf
 class KeyPointClassifier(object):
     def __init__(
         self,
-        model_path='model/keypoint_classifier/keypoint_classifier.tflite',
+        model_path='model/keypoint_classifier/keypoint_classifier.keras',
         num_threads=1,
     ):
-        self.interpreter = tf.lite.Interpreter(model_path=model_path,
-                                               num_threads=num_threads)
-
-        self.interpreter.allocate_tensors()
-        self.input_details = self.interpreter.get_input_details()
-        self.output_details = self.interpreter.get_output_details()
+        self.model = tf.keras.models.load_model(model_path, compile=False)
 
     def __call__(
         self,
         landmark_list,
     ):
-        input_details_tensor_index = self.input_details[0]['index']
-        self.interpreter.set_tensor(
-            input_details_tensor_index,
-            np.array([landmark_list], dtype=np.float32))
-        self.interpreter.invoke()
-
-        output_details_tensor_index = self.output_details[0]['index']
-
-        result = self.interpreter.get_tensor(output_details_tensor_index)
-
-        result_index = np.argmax(np.squeeze(result))
-
-        return result_index
+        x = np.array([landmark_list], dtype=np.float32)
+        y = self.model.predict(x, verbose=0)
+        return int(np.argmax(np.squeeze(y)))
